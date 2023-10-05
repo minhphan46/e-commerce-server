20d4ae7b3cbf51a4a7f92e75b8d27b7c87f10b36
minhphan46 <21522345@gm.uit.edu.vn>
2023-10-06 00:01:01 +0700

    connect db

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const connectDB = require("./db/connect");
+
+app.get("/", (req, res) => res.send("Hello World!"));
+// start
+const port = process.env.PORT || 3000;
+const start = async () => {
+  try {
+    // connect to database
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server listening on port ${port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+start();
